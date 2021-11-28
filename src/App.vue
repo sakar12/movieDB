9a9c58b2acb2196ea02dc455e2d9d02939f0a567
sk12r <sakar.kc12@gmail.com>
2021-11-29 00:16:28 +0700

    Setup project, added nav drawer,app bar and image

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app> 
+    <v-navigation-drawer v-model="drawer" app fixed color="black" dark>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6"> Movie DB </v-list-item-title>
+          <v-list-item-subtitle> Movies and Tv show information </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item-group>
+          <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            link
+            color="black"
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="black " dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Movie DB</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  
+  name: "App",
+
+  data: () => ({
+    drawer: false,
+    items: [
+      { title: "Home", icon: "mdi-home" },
+      { title: "Movies", icon: "mdi-movie-play" },
+      { title: "Tv Shows", icon: "mdi-movie-star" },
+      { title: "History", icon: "mdi-history" },
+    ],
+    right: null,
+  }),
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,57 @@
+<template>
+<div style="background-color:black;">
+  <v-container  fluid>
+    <v-img
+      contain
+      src="https://wallpapermemory.com/uploads/262/the-dark-knight-rises-wallpaper-hd-1080p-161226.jpg"
+      id="img"
+      class="mt-n3"
+    />
+
+    <!-- <v-layout row wrap>
+  <v-flex xs12 sm12>
+    <v-parallax src="https://cdn.vuetifyjs.com/images/parallax/material.jpg">
+      <v-layout column align-center justify-center>
+        <h1>title</h1>
+      </v-layout>
+    </v-parallax>
+  </v-flex>
+</v-layout> -->
+    <!-- <v-parallax
+      class="mt-n3"
+      
+      jumbotron
+      
+    ></v-parallax> -->
+  </v-container>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+
+  data: () => ({
+    drawer: false,
+    items: [
+      { title: "Home", icon: "mdi-home" },
+      { title: "Movies", icon: "mdi-movie-play" },
+      { title: "Tv Shows", icon: "mdi-movie-star" },
+      { title: "History", icon: "mdi-history" },
+    ],
+    right: null,
+  }),
+
+  components: {},
+};
+</script>
+<style scoped>
+#img {
+  width: 100%;
+  height: 100%;
+}
+
+body {
+    background: orange;
+}
+</style>
